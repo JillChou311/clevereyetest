18d6190e6d5979251d00aa32d20419ca40bfce5f
jill_chou <jill_chou@gemteks.com>
2018-03-13 14:09:04 +0800

    add client info

---
+++ online_tool/client2.js
@@ -0,0 +1,39 @@
+
+/* server setting*/
+var MQTT_HOST = "s5-ns001.securepilot.com";
+var MQTT_PORT = 80;
+var SERVICE = "CVR";
+
+/* client info settng*/
+var CLIENT_ID = '600097052';
+var CLIENT_USERNAME = '600097052';
+var CLIENT_PWD = '83551832';
+
+/* MQTT topic*/
+var client_topic = 'client/' + CLIENT_ID + '/' + CLIENT_ID + '-' + SERVICE;
+var system_topic = 'client/' + CLIENT_ID + '/' + CLIENT_ID + '-SYS';
+
+var mqtt = require('mqtt');
+var client  = mqtt.connect({'port': MQTT_PORT, 'host': MQTT_HOST,
+                            'protocolId': 'MQIsdp', 'protocolVersion': 3, cmd: 'connect', 'clientId': CLIENT_ID,
+                            'username': CLIENT_USERNAME, 'password': CLIENT_PWD});
+
+client.on('connect', function () {
+    console.log ('connect');
+    client.subscribe(system_topic);
+    client.subscribe(client_topic);
+    client.publish('@SYS/notify/online', '{"client":' + '"' + CLIENT_ID + '-test-PC"}');
+});
+ 
+client.on('message', function (topic, message) {
+    // message is Buffer 
+    console.log ('--------------------------');
+    console.log('topic: ' +  topic);
+    console.log(message.toString());
+    console.log(new Buffer(JSON.parse(message.toString()).content, 'base64').toString());
+    console.log ('--------------------------');
+});
+
+client.on('error', function (e) {
+    console.error ('error: ' + JSON.stringify(e)); 
+});
