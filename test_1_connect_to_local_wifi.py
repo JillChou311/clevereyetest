3edb489e7dd136fb691bcc11f66963d3ff7d229b
jill_chou <jill_chou@gemteks.com>
2018-07-05 11:14:18 +0800

    remove the wifi setting

--- test_1_connect_to_local_wifi.py
+++ test_1_connect_to_local_wifi.py
@@ -34,7 +34,7 @@ class wifi_test(unittest.TestCase):
     # initialize
 
     def setUp(self):
-        os.system('adb -s EAAZCY17E701 shell am start -n io.appium.settings/.Settings -e wifi on')
+        # os.system('adb -s EAAZCY17E701 shell am start -n io.appium.settings/.Settings -e wifi on')
         sleep(50)
         desired_caps = {
             'platformName': 'Android',
@@ -54,49 +54,50 @@ class wifi_test(unittest.TestCase):
     # Test script
 
     def test_1_wifiSet(self):
-        elem = self.driver.find_elements_by_id("android:id/title")
-        for e in elem:
-            if e.text == u"Wi‑Fi":
-                e.click()
-                sleep(2)
-                break
-
-        self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
-
-        elem = self.driver.find_elements_by_class_name('android.widget.TextView')
-        connect_status_fail = True
-        for e in elem:
-            if e.text == u'已連線':
-                connect_status_fail = False
-                print 'local already connect.'
-                break
-            else:
-                continue
-
-        if connect_status_fail:
-            element = self.driver.find_elements_by_class_name("android.widget.Button")
-            for e in element:
-                if e.text == u'連線':
-                    e.click()
-
-            sleep(15)
-            self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
-            elem = self.driver.find_elements_by_class_name('android.widget.TextView')
-            connect_status = False
-            for e in elem:
-                if e.text == u'已連線':
-                    connect_status = True
-                    break
-                else:
-                    continue
-                
-            if connect_status:
-                sleep(2)
-            else:
-                timestr2 = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
-                self.driver.get_screenshot_as_file("screen"+timestr2+".png")           
-
-            assert connect_status,'Connect to local fail.'
+        os.system('adb -s EAAZCY17E701 shell am start -n io.appium.settings/.Settings -e wifi off')
+        # elem = self.driver.find_elements_by_id("android:id/title")
+        # for e in elem:
+        #     if e.text == u"Wi‑Fi":
+        #         e.click()
+        #         sleep(2)
+        #         break
+        #
+        # self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+        #
+        # elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+        # connect_status_fail = True
+        # for e in elem:
+        #     if e.text == u'已連線':
+        #         connect_status_fail = False
+        #         print 'local already connect.'
+        #         break
+        #     else:
+        #         continue
+        #
+        # if connect_status_fail:
+        #     element = self.driver.find_elements_by_class_name("android.widget.Button")
+        #     for e in element:
+        #         if e.text == u'連線':
+        #             e.click()
+        #
+        #     sleep(15)
+        #     self._ScanSSID("DropAP-0bb068", 'android:id/title', 1000, 200)
+        #     elem = self.driver.find_elements_by_class_name('android.widget.TextView')
+        #     connect_status = False
+        #     for e in elem:
+        #         if e.text == u'已連線':
+        #             connect_status = True
+        #             break
+        #         else:
+        #             continue
+        #
+        #     if connect_status:
+        #         sleep(2)
+        #     else:
+        #         timestr2 = time.strftime('%Y%m%d%H%M%S',time.localtime(time.time()))
+        #         self.driver.get_screenshot_as_file("screen"+timestr2+".png")
+        #
+        #     assert connect_status,'Connect to local fail.'
 
 
 if __name__ == '__main__':
